826891c20fba362a356e96bdcfec7bbb5cb721c8
Eric Kuhn <eskuhn@gmail.com>
2014-02-18 09:30:29 -0800

    changes to pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
   has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" },
   :storage => :s3,
-  :bucket => 'ekrailstest'
+  :bucket => ENV['AWS_BUCKET'],
   :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
 end
