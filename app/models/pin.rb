9552edafafae14752aa50d64a708b106b349f8d8
Eric Kuhn <eskuhn@gmail.com>
2014-02-18 13:37:50 -0800

    added jquery masonry

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-  has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" },
-  :storage => :s3,
-  :bucket => ENV['AWS_BUCKET'],
-  :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  has_attached_file :image , 
+  :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
 end
